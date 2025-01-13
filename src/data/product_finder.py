bb3412c655c347429d3feb526f832e60a212c869
Estevan <estevancantuaria23@gmail.com>
2025-01-12 21:52:07 -0300

    feat: Implementing http types

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -1,8 +1,12 @@
 from src.models.sqlite.repository.products_repository import ProductsRepository
 from src.models.redis.repository.redis_repository import RedisRepository
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+
 class ProductFinder:
     def __init__(self, repository: ProductsRepository, redis_repo: RedisRepository) -> None:
         self.__redis_repo = redis_repo
         self.__products_repo = repository
         
-        
+    def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
+        pass
--- src/models/redis/repository/interfaces/redis_repository.py
+++ src/models/redis/repository/interfaces/redis_repository.py
@@ -9,19 +9,19 @@ class RedisRepositoryInterface(ABC):
     @abstractmethod
     def get_key(self, key: str) -> str:
         pass
-  
+
     @abstractmethod
     def insert_hash(self, key: str, field: str, value: any) -> None:
         pass
-        
+
     @abstractmethod
     def get_hash(self, key: str, field: str) -> any:
-        pass        
+        pass
 
     @abstractmethod
     def insert_ex(self,key:str,field:str, value:any, ex:int) -> None:
         pass
-    
+
     @abstractmethod
     def insert_hash_ex(self, key:str, field:str, value:any, ex:int) -> None:
         pass
